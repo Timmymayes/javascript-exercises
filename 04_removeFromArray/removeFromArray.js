e157a6e6a05c036ce73fa2db0185011bbd996987
Tyler Mayes <timmymayes@gmail.com>
2022-06-03 00:04:55 -0700

    Complete Lesson 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(collection, ...toRemove) {
 
+  toRemove.forEach(itemToIsolate => {
+    while (collection.includes(itemToIsolate)) {
+      collection.splice(collection.indexOf(itemToIsolate), 1);
+  }
+    
+  })
+         return collection;
 };
 
 // Do not edit below this line
