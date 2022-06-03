83326b30828179c202ce3071eefd6e61f9172447
Tyler Mayes <timmymayes@gmail.com>
2022-06-02 22:55:38 -0700

    Complete Lesson 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let charArray = Array.from(string);
+  charArray.reverse();
+  return charArray.join('');
 };
 
 // Do not edit below this line
