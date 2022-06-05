735c287c859e6c9018b8f776d01603e1f2c50d31
Tyler Mayes <timmymayes@gmail.com>
2022-06-04 23:43:49 -0700

    Complete Lesson 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,31 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  if (typeof(tempF) != 'number') {
+    return 'ERROR';
+  }
+  let num = trim((tempF - 32) * 5/9, 1);
+  return num;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  if (typeof(tempC) != 'number'){
+    return 'ERROR';
+  }
+  let num = trim(tempC * 9/5 + 32, 1);
+  return num;
 };
+
+function trim(numToTrim, maxDecimals) {
+  if ( Number.isInteger(numToTrim)) {
+    return numToTrim;
+  }
+  return parseFloat(numToTrim.toFixed(maxDecimals));
+}
+
+
+
+//	[°F] = [°C] × 9⁄5 + 32
+//      [°C] = ([°F] − 32) × 5⁄9
+
 
 // Do not edit below this line
 module.exports = {
