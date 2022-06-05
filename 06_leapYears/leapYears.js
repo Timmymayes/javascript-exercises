16a74071c6ac62eab31cd53966134b2c0275b533
Tyler Mayes <timmymayes@gmail.com>
2022-06-04 21:51:20 -0700

    Complete Lesson 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  //leap years are divisible by 4, except when divisible by 100, unless they are divisible by 400.
 
+  if ( year % 4 === 0 && year % 100 != 0 || year % 400 === 0 ) {
+    return true;
+  }
+  return false;
+  
 };
 
 // Do not edit below this line
