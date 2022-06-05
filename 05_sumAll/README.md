25e067f9a83a003ae4235b7e32ee2703ecd01b6d
Tyler Mayes <timmymayes@gmail.com>
2022-06-04 21:42:16 -0700

    Complete Exercise 05

--- 05_sumAll/README.md
+++ 05_sumAll/README.md
@@ -15,3 +15,13 @@ Think about how you would do this on pen and paper and then how you might transl
 - loop through the given numbers ([link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration))
 - on each iteration add the number to the sum
 - return the sum after finishing the loop
+
+
+5,3
+target = 12 (5+4+3)
+3,5
+gap = 2
+sum start = 8
+
+
+
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+  if (typeof(numberOne) != 'number' || typeof(numberTwo) != 'number' || numberOne < 0 || numberTwo < 0 ){
+    return 'ERROR';
+  }
+  let sum;
+  let bounds = [numberOne, numberTwo];
+  bounds.sort();
+  let gap = bounds[1] - bounds[0];
+  sum = bounds[0]+bounds[1];
+  while (gap > 1) {
+    gap--; 
+    sum += bounds[0]+gap;
 
+  }
+  return sum;
+
+  
 };
 
 // Do not edit below this line
