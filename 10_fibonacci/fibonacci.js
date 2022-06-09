47448f4e25e6f40730f7344c86f76db6d4c6a905
Tyler Mayes <timmymayes@gmail.com>
2022-06-08 19:40:24 -0700

    Complete lesson 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(targetInSequence) {
+  //A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+  if (targetInSequence < 0)  {
+    return 'OOPS'
+  }
+  targetInSequence = parseInt(targetInSequence);
+  let index = 0;
+  let currentValue = 0;
+  let nextValue = 1;
+  let nextNextValue = 0;
 
+  do {
+    nextNextValue = currentValue + nextValue;
+    currentValue = nextValue;
+    nextValue = nextNextValue;    
+    index++;
+  } while ( index < targetInSequence );
+
+  return currentValue;
+  
+  
+
+  
 };
 
 // Do not edit below this line
